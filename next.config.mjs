169bc59e972483d424ce4b93e1f17eb0b236c26f
Siddhant Mohile <meetsiddhant@gmail.com>
2024-07-07 10:31:01 +0530

    Migrated API to nextJS and added mock api and fixed bugs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
+import { config } from 'dotenv';
+import { join } from 'path';
+
+// Load environment variables from .env.local
+config({ path: join(process.cwd(), '.env.local') });
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    serverRuntimeConfig: {
+        apiKey: process.env.API_KEY,
+    },
+    publicRuntimeConfig: {
+        apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    },
+};
 
 export default nextConfig;
