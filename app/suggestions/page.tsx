b78ed94fbd97c61e99b79451183bcaddb2323154
Siddhant Mohile <meetsiddhant@gmail.com>
2024-07-08 20:52:38 +0530

    Added place holder and changed config. Added background effects aswell as accenture prerequisites

--- app/suggestions/page.tsx
+++ app/suggestions/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+// pages/index.tsx
 
-const page = () => {
+import FeedbackForm from '@/components/FeedbackForm';
+import { BackgroundBeams } from "@/components/ui/background-beams";
+
+const Home = () => {
   return (
-    <div>pee pee</div>
-  )
-}
+    <div className="min-h-screen bg-offwhite flex items-center justify-center relative">
+      <BackgroundBeams />
+      <div className="relative z-10 p-4 bg-white shadow-md rounded-md">
+        <FeedbackForm />
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default Home;
---
+++ data/constants.ts
@@ -0,0 +1,26 @@
+export const name_placeholders = [
+    "Siddhant",
+    "Jason",
+    "Arnav"
+  ];
+
+export const feature_placeholders = [
+    "Add an option to change the voice of the podcast narrator",
+    "Allow users to download podcasts",
+    "Add translations for the newsletters"
+]
+
+export const newsletter_placeholders = [
+    "Morning Brew",
+    "The Hustle",
+    "SaaS Weekly",
+    "Product Hunt",
+    "The Saturday Solopreneur"
+]
+
+export const price_placeholders = [
+    "$5 a month",
+    "I'm broke lol",
+    "₹199 a month",
+    "₹400 a year"
+]
