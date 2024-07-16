d53238cff04488604df8564c4caa8c05aa5d17b6
Zero_& _One <zeronone13.5@gmail.com>
2024-07-16 15:07:41 +0530

    Changes Features Effects and Added Link Preview in Footer

--- components/FeaturesGallery.tsx
+++ components/FeaturesGallery.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+"use client";
+
 import NextImage from "next/image";
-import { motion } from "framer-motion";
-import { cn } from "@/utils/cn";
-import { TABS } from "@/data/constants"
+import React from "react";
+import { CardBody, CardContainer, CardItem } from "@/components/ui/3dCard";
+import { TABS } from "@/data/constants";
 
 interface Tab {
   title: string;
@@ -12,79 +13,43 @@ interface Tab {
   secondColor: string;
 }
 
-const HoverTab = ({ tab, isActive, onTabClick }: { 
-  tab: Tab; 
-  isActive: boolean; 
-  onTabClick: (tabIndex: number) => void;
-}) => {
-  const [hovered, setHovered] = useState(false);
-
-  return (
-    <motion.div
-      className={cn(
-        "relative group block p-4 rounded-lg shadow-md cursor-pointer",
-        "bg-gray-800 text-white", // Dark background and light text by default
-        isActive ? `bg-gray-800 border-2 border-gray-500` : "bg-gray-800", // Active or default background color
-        hovered && !isActive && "bg-gray-700", // Darken background on hover if not active
-      )}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
-      onClick={() => onTabClick(TABS.findIndex(t => t.title === tab.title))}
-    >
-      <div className="flex items-center">
-        <div className={`w-12 h-12 rounded-full ${tab.baseColor} mr-4`} />
-        <h4 className="text-lg font-bold">{tab.title}</h4>
-      </div>
-      {isActive && (
-        <motion.p
-          className="mt-4 text-gray-200"
-          initial={{ height: 0, opacity: 0, translateY: 20 }} // Initial height and opacity
-          animate={{ height: "auto", opacity: 1, translateY: 0 }} // Animate to full height and opacity
-          exit={{ height: 0, opacity: 0, translateY: -20 }} // Exit animation
-          transition={{ duration: 0.5 }} // Animation duration
-        >
-          {tab.description}
-        </motion.p>
-      )}
-    </motion.div>
-  );
-};
-
 export default function FeaturesGallery() {
-  const [currentTab, setCurrentTab] = useState<Tab>(TABS[0]);
-
-  const handleTabClick = (tabIndex: number) => {
-    setCurrentTab(TABS[tabIndex]);
-  };
-
   return (
     <div className="container mx-auto py-12 text-center">
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-        <div className="md:col-span-1">
-          <div className="grid grid-cols-1 gap-8">
-            {TABS.map((tab, idx) => (
-              <HoverTab
-                key={idx}
-                tab={tab}
-                isActive={tab.title === currentTab.title}
-                onTabClick={handleTabClick}
-              />
-            ))}
-          </div>
-        </div>
-        <div className="md:col-span-2">
-          <motion.div
-            className="relative h-full"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.5 }} // Animation duration
-          >
-            <NextImage src={currentTab.imageUrl} alt={currentTab.title} layout="fill" objectFit="cover" />
-          </motion.div>
-        </div>
+      <div className="flex flex-wrap justify-center gap-8">
+        {TABS.map((tab, idx) => (
+          <CardContainer key={idx} className="inter-var mx-4 my-4">
+            <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
+              <CardItem
+                translateZ="50"
+                className="text-xl font-bold text-neutral-600 dark:text-white"
+              >
+                {tab.title}
+              </CardItem>
+              <CardItem
+                as="p"
+                translateZ="60"
+                className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+              >
+                {tab.description}
+              </CardItem>
+              <CardItem
+                translateZ="100"
+                rotateX={20}
+                rotateZ={-10}
+                className="w-full mt-4"
+              >
+                <NextImage
+                  src={tab.imageUrl}
+                  height="1000"
+                  width="1000"
+                  className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                  alt={tab.title}
+                />
+              </CardItem>
+            </CardBody>
+          </CardContainer>
+        ))}
       </div>
     </div>
   );
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,5 @@
-import NextImage from "next/image";
+import Image from "next/image";
+import { LinkPreview } from "@/components/ui/link-preview";
 
 export default function Footer() {
   return (
@@ -6,16 +7,21 @@ export default function Footer() {
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center space-x-4">
           <div className="relative w-8 h-8">
-            <NextImage src="/nsp_logo.jpg" alt="Product Logo" layout="fill" objectFit="contain" />
+            <Image src="/nsp_logo.jpg" alt="Product Logo" width={32} height={32}/>
           </div>
           <span className="text-sm">&copy; 2024 NewsPodify. All rights reserved.</span>
         </div>
 
         <div className="flex items-center space-x-4">
-          <span className="text-sm">ZeroAndOne Developers</span>
-          {/* Company Logo */}
+          <span className="text-sm">
+          <LinkPreview
+            url="https://www.zodevelopers.com"
+            className="font-bold bg-clip-text text-transparent bg-gradient-to-br from-purple-500 to-pink-500">
+              ZeroAndOne Developers
+          </LinkPreview>
+          </span>
           <div className="relative w-8 h-8">
-            <NextImage src="/zo_logo.png" alt="Company Logo" layout="fill" objectFit="contain" />
+            <Image src="/zo_logo.png" alt="Company Logo" width={32} height={32}/>
           </div>
         </div>
       </div>
