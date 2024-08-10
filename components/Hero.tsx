a9f25d8f0b6697e666b6e10e1f1997171c1409bf
Zero_& _One <zeronone13.5@gmail.com>
2024-08-10 17:32:17 +0530

    Initial commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -47,7 +47,7 @@ const Hero: React.FC = () => {
                 <div className="flex flex-col md:flex-row md:space-x-4 mt-6 md:mt-8 items-center justify-center"> {/* Updated flex styling */}
                     <Link href="/main" className="flex items-center bg-slate-400 text-slate-50 py-3 px-6 rounded-full hover:bg-gray-700 hover:scale-95 transition-all text-lg md:text-xl mb-4 md:mb-0">
                         <FaHourglassEnd className="mr-2" />
-                        Join Waitlist Now
+                        Try Version 0.0.1
                     </Link>
                     <Link href="#description" className="flex items-center bg-blue-500 text-white py-3 px-6 rounded-full hover:bg-blue-700 hover:scale-95 transition-all text-lg md:text-xl">
                         <FaInfoCircle className="mr-2" />
